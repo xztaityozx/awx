afb0f5bbaa022ed59153d84b074069a55db541bc
xztaityozx <taityonohibi@gmail.com>
2018-10-05 22:46:16 +0900

    project inited

---
+++ cmd/version.go
@@ -0,0 +1,43 @@
+// Copyright © 2018 xztaityozx
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/xztaityozx/go-version"
+)
+
+var AwxVersion version.Version = version.New(0, 0, 1, version.BETA, "2018/10/05")
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: `Version情報を出力して終了します`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("awx %s\n", AwxVersion.ToString())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
